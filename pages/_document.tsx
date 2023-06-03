7a22b5c55a7b1e41399e83b45ba93974f260ebed
Kylian Leroy <aleexisazul@gmail.com>
2023-06-03 16:38:28 -0400

    trg lol

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,32 +1,36 @@
-import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document'
-import Link from 'next/link'
-
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from "next/document";
+import Link from "next/link";
 
 class MyDocument extends Document {
-    static async getInitialProps(ctx: DocumentContext) {
-        const initalProps = await Document.getInitialProps(ctx)
+  static async getInitialProps(ctx: DocumentContext) {
+    const initalProps = await Document.getInitialProps(ctx);
 
-        return initalProps
-    }
+    return initalProps;
+  }
 
-    render() {
-        return (
-            <Html>
-                <Head>
-                
-                    <meta name="description" content="A site for my programming portfolio" />
-                    <link rel="icon" href="/favicon.ico" />
-
-                    
-                                       
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta
+            name="description"
+            content="A site for my programming portfolio"
+          />
+          <Link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
-export default MyDocument
+export default MyDocument;
